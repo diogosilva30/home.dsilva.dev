abf57210e40dd771f02246617e6faf68a6c196b1
Diogo Silva <diogosilv30@gmail.com>
2023-03-05 19:04:25 +0000

    fix ports for nginx and tunnel

--- terraform/tunnel_config.tf
+++ terraform/tunnel_config.tf
@@ -1,14 +1,14 @@
 # Generates the cloudflared tunnel config yaml file
 resource "local_file" "tunnel_config" {
   content = <<-DOC
-    tunnel: ${cloudflare_tunnel.tunnel.id}
+    tunnel: "${cloudflare_tunnel.tunnel.id}"
     credentials-file: /etc/cloudflared/cert.json
     logfile: /var/log/cloudflared.log
     loglevel: info
     # Define ingress rules
     ingress:
-        - hostname: ${var.hostname}
-          service: http://localhost:8213 # default port for home assistant
+        - hostname: '${var.hostname}'
+          service: http://localhost:8123 # default port for home assistant
         # Default catch-all rule
         - service: http_status:404
     DOC
